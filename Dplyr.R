89691103eea24da368843c3897220ef8a49c0b34
Smeet Mehta <59577058+SmeetMehta@users.noreply.github.com>
2021-09-09 00:43:25 +0530

    Update Dplyr.R

--- Dplyr.R
+++ Dplyr.R
@@ -5,3 +5,5 @@ slice(flights,1:10) # allows to slice through the data
 arrange(flights,year,month,day,air_time)
 #similar to order-by. this line will first order by year,then by month,then by day and then by air_time
 arrange(flights,year,desc(month),day,air_time) # this will arrange everything in ascending order except month
+
+select(flights,year,month,day) #this will select the given columns
