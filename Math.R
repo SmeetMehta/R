7325721dc76ac98a70edf18f0ed460e94d18d8ae
Smeet Mehta <59577058+SmeetMehta@users.noreply.github.com>
2021-09-08 23:49:11 +0530

    Update Math.R

--- Math.R
+++ Math.R
@@ -1,1 +1,10 @@
 print(abs(-2)) # absolute function
+v <- c(-1,-2,-4,4,5)
+print(abs(v))
+
+num <- 2.34232432
+print(round(num, digits=3)) #rounf function
+
+# all the other notes are present in the link below
+
+https://cran.r-project.org/doc/contrib/Short-refcard.pdf
