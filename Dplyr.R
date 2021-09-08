c3cb247b5f045a437545e22370683c3b69500b94
Smeet Mehta <59577058+SmeetMehta@users.noreply.github.com>
2021-09-09 00:40:40 +0530

    Update Dplyr.R

--- Dplyr.R
+++ Dplyr.R
@@ -1,3 +1,6 @@
 filter(flights,month==11,day==3,carrier=="AA") # directy filers the data easily
 
 slice(flights,1:10) # allows to slice through the data
+
+arrange(flights,year,month,day,air_time)
+#similar to order-by. this line will first order by year,then by month,then by day and then by air_time
