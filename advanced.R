d622c200892698ef57e16e5e7712712ca3c167e8
Smeet Mehta <59577058+SmeetMehta@users.noreply.github.com>
2021-09-08 19:39:00 +0530

    Update advanced.R

--- advanced.R
+++ advanced.R
@@ -1,1 +1,10 @@
 seq(0,10,by=2) #sequence with a jump of 2
+
+v <- c(23,3,45,3,2,233)  # Vector
+print("sorted v")
+sort(v)  # to sort a given vector.
+
+print("Sorted v in decreasing order")
+sort(v,decreasing =T) # to sort a given vector in decreadsing order.
+
+
