96ec67e5159b906092c4f91c61dfe1fabb5303ff
Smeet Mehta <59577058+SmeetMehta@users.noreply.github.com>
2021-09-08 23:34:17 +0530

    Create apply.R

---
+++ apply.R
@@ -0,0 +1,13 @@
+v <- c(1,2,3,4,5)
+
+ra <- function(x){
+  ran <- sample(1:100,1)
+  return(x+ran)
+}
+
+res <- lapply(v,ra) # will return a list
+print(res)
+
+
+res <- sapply(v,ra) # will return a vector or list(whatever is used in input)
+print(res)
